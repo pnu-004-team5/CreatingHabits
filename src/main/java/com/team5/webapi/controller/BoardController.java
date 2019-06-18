1e2a63c037137528d7a0a52e1114656c4318039b
SpongeCake <sunrivs@gmail.com>
2019-06-18 17:28:44 +0900

    sonar

--- src/main/java/com/team5/webapi/controller/BoardController.java
+++ src/main/java/com/team5/webapi/controller/BoardController.java
@@ -14,10 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.team5.webapi.model.BoardComment;
 import com.team5.webapi.model.BoardDocument;
-import com.team5.webapi.model.Habit;
 import com.team5.webapi.repository.BoardCommentRepository;
 import com.team5.webapi.repository.BoardDocumentRepository;
-import com.team5.webapi.repository.HabitRepository;;
 
 @RestController
 public class BoardController {
@@ -29,8 +27,7 @@ public class BoardController {
   @RequestMapping(value = "/board/documents", method = { RequestMethod.GET }, produces = "application/json")
   public List<BoardDocument> getBoardDocuments(BoardDocument boardDocument) {
     List<BoardDocument> boardDocuments;
-    System.out.println("-----------------------");
-    System.out.println(boardDocument.getBoard());
+    
     if(boardDocument.getBoard().equals("total"))
       boardDocuments = boardDocumentRepository.findAllByOrderByIdDesc();
     else 
@@ -42,8 +39,7 @@ public class BoardController {
 
   @RequestMapping(value = "/board/document", method = { RequestMethod.POST }, produces = "application/json")
   public BoardDocument createBoardDocument(BoardDocument boardDocument) {
-    // boardDocument.setContent("안녕하세요");
-    // boardDocument.setVideoUrl();
+    
 
     BoardDocument boardDocumentData = boardDocumentRepository.save(boardDocument);
 
@@ -92,64 +88,11 @@ public class BoardController {
 
   @RequestMapping(value = "/board/comment", method = { RequestMethod.POST }, produces = "application/json")
   public BoardComment createBoardDocument(BoardComment boardComment) {
-    // boardDocument.setContent("안녕하세요");
-    // boardDocument.setVideoUrl();
+    
 
-    BoardComment boardCommentData = boardCommentRepository.save(boardComment);
-
-    return boardCommentData;
+    return boardCommentRepository.save(boardComment);
   }
 
 
-
-
-
-  // @RequestMapping(value = "/habit", method = { RequestMethod.POST })
-  // public Habit addHabit(Habit habit) {
-  //   ArrayList<String> colors = new ArrayList<>();
-  //   colors.add("#e57373");
-  //   colors.add("#f06292");
-  //   colors.add("#ba68c8");
-  //   colors.add("#9575cd");
-  //   colors.add("#4dd0e1");
-  //   colors.add("#81c784");
-  //   colors.add("#90a4ae");
-  //   colors.add("#ff8a65");
-  //   Random rand = new Random();
-  //   int n = rand.nextInt(colors.size());
-  //   habit.setColor(colors.get(n));
-  //   habit.setCompleteDate("[]");
-
-  //   Habit habitData = habitRepository.save(habit);
-
-  //   return habitData;
-  // } 
-
-  // @RequestMapping(value = "/habit", method = { RequestMethod.PUT })
-  // public Habit doneHabit(int id, String targetDate) {
-  //   Habit habitData = habitRepository.findById(id).get();
-    
-  //   try {
-  //     JSONArray completeDateJsonArr = new JSONArray(habitData.getCompleteDate());
-  //     int found = -1;
-  //     for(int i = 0; i < completeDateJsonArr.length(); i++) {
-  //       if(completeDateJsonArr.getString(i).equals(targetDate)) {
-  //         found = i;
-  //       }
-  //     }
-      
-  //     if(found > -1)
-  //       completeDateJsonArr.remove(found);
-  //     else
-  //       completeDateJsonArr.put(targetDate);
-
-  //     habitRepository.done(id, completeDateJsonArr.toString());
-  //     habitData.setCompleteDate(completeDateJsonArr.toString());
-  //   } catch(Exception e) {
-  //     e.printStackTrace();
-  //   }
-
-  //   return habitData;
-  // }
 
 }
